7c51d9bc4c49d3a8bd886a5d3e6b020418376c86
bhattiasad12 <bhattiasad12@gmail.com>
2024-10-04 20:24:42 +0500

    handle no image placeholder

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import no_image_placeholder from "../assets/no_image_placeholder.avif";
+
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return no_image_placeholder;
   const target = "media/";
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
