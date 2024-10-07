79a92a3bd9809ca6652388148b86e13684df3e45
bhattiasad12 <bhattiasad12@gmail.com>
2024-10-08 03:20:09 +0500

    removed duplicate interface

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -10,8 +10,8 @@ import { BsNintendoSwitch } from "react-icons/bs";
 import { MdPhoneIphone } from "react-icons/md";
 import { BsGlobe } from "react-icons/bs";
 import { HStack, Icon } from "@chakra-ui/react";
-import { Platform } from "../hooks/useGames";
 import { IconType } from "react-icons";
+import { Platform } from "../hooks/usePlatforms";
 
 interface Props {
   platforms: Platform[];
