bb7636bf813d50790efec22b282dc9e1779220b1
bhattiasad12 <bhattiasad12@gmail.com>
2024-10-08 11:59:53 +0500

    react infinite scroll component

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -32,6 +32,7 @@ const useGames = (gameQuery: GameQuery) =>
     getNextPageParam: (lastPage, Allpages) => {
       return lastPage.next ? Allpages.length + 1 : undefined;
     },
+    staleTime: 24 * 60 * 60 * 1000, //24hrs
   });
 
 export default useGames;
